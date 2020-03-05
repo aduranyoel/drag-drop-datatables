4bdbc77655ffb57b4a9e09b9d6a7ba009bcf4ea4
Tony Duran <yoet92@gmail.com>
2020-03-05 13:37:12 -0300

    select multiple

--- main.js
+++ main.js
@@ -1,7 +1,5 @@
-var dragSrcRow = null;  // Keep track of the source row
-var srcTable = '';  // Global tracking of table being dragged for 'over' class setting
-var rows = [];   // Global rows for #example
-var rows2 = [];  // Global rows for #example2
+var dragSrcRow = null;  // Informacion que se envia
+var srcTable = '';  // Tabla de origen
 
 
 const data = [
@@ -49,197 +47,9 @@ const data = [
         "fechaAlta": "2014-08-25 17:56:07.0",
         "flagEstado": "",
         "id": 79666719
-    },
-    {
-        "codigoDefecto": "1453",
-        "codigoFamilia": "ASF",
-        "descDefecto": "PUNTAS DESALINEADAS",
-        "descFamilia": "Aspecto Físico",
-        "fechaAlta": "2014-08-25 17:56:07.0",
-        "flagEstado": "",
-        "id": 79666738
-    },
-    {
-        "codigoDefecto": "1457",
-        "codigoFamilia": "ASF",
-        "descDefecto": "ROLLO CORTADO",
-        "descFamilia": "Aspecto Físico",
-        "fechaAlta": "2014-08-25 17:56:07.0",
-        "flagEstado": "",
-        "id": 79666740
-    },
-    {
-        "codigoDefecto": "1458",
-        "codigoFamilia": "ASF",
-        "descDefecto": "ROLLO EMPALMADO",
-        "descFamilia": "Aspecto Físico",
-        "fechaAlta": "2014-08-25 17:56:07.0",
-        "flagEstado": "",
-        "id": 79666741
-    },
-    {
-        "codigoDefecto": "902",
-        "codigoFamilia": "ASF",
-        "descDefecto": "CAMBIO DE ESPESOR",
-        "descFamilia": "Aspecto Físico",
-        "fechaAlta": "",
-        "flagEstado": "",
-        "id": 333
-    },
-    {
-        "codigoDefecto": "7",
-        "codigoFamilia": "ASF",
-        "descDefecto": "MALTRATO",
-        "descFamilia": "Aspecto Físico",
-        "fechaAlta": "2011-10-17 15:10:29.0",
-        "flagEstado": "",
-        "id": 5
-    },
-    {
-        "codigoDefecto": "9",
-        "codigoFamilia": "ASF",
-        "descDefecto": "MOJADO",
-        "descFamilia": "Aspecto Físico",
-        "fechaAlta": "2011-10-17 15:10:29.0",
-        "flagEstado": "",
-        "id": 7
-    },
-    {
-        "codigoDefecto": "35",
-        "codigoFamilia": "ASF",
-        "descDefecto": "ROLLO ENVOLVENTE PROTECTOR",
-        "descFamilia": "Aspecto Físico",
-        "fechaAlta": "2011-10-17 15:10:29.0",
-        "flagEstado": "",
-        "id": 31
-    },
-    {
-        "codigoDefecto": "71",
-        "codigoFamilia": "ASF",
-        "descDefecto": "EMPAQUE",
-        "descFamilia": "Aspecto Físico",
-        "fechaAlta": "2011-10-17 15:10:29.0",
-        "flagEstado": "",
-        "id": 58
-    },
-    {
-        "codigoDefecto": "73",
-        "codigoFamilia": "ASF",
-        "descDefecto": "MAL ENROLLADO",
-        "descFamilia": "Aspecto Físico",
-        "fechaAlta": "2011-10-17 15:10:29.0",
-        "flagEstado": "",
-        "id": 60
-    },
-    {
-        "codigoDefecto": "75",
-        "codigoFamilia": "ASF",
-        "descDefecto": "RECHUPE (ROLLO)",
-        "descFamilia": "Aspecto Físico",
-        "fechaAlta": "2011-10-17 15:10:29.0",
-        "flagEstado": "",
-        "id": 62
-    },
-    {
-        "codigoDefecto": "78",
-        "codigoFamilia": "ASF",
-        "descDefecto": "TENSIÓN DE ENROLLADO",
-        "descFamilia": "Aspecto Físico",
-        "fechaAlta": "2011-10-17 15:10:29.0",
-        "flagEstado": "",
-        "id": 65
-    },
-    {
-        "codigoDefecto": "80",
-        "codigoFamilia": "ASF",
-        "descDefecto": "80",
-        "descFamilia": "Aspecto Físico",
-        "fechaAlta": "",
-        "flagEstado": "",
-        "id": 66
-    },
-    {
-        "codigoDefecto": "81",
-        "codigoFamilia": "ASF",
-        "descDefecto": "TELESCOPIADO",
-        "descFamilia": "Aspecto Físico",
-        "fechaAlta": "2011-10-17 15:10:29.0",
-        "flagEstado": "",
-        "id": 67
-    },
-    {
-        "codigoDefecto": "82",
-        "codigoFamilia": "ASF",
-        "descDefecto": "COLEADO",
-        "descFamilia": "Aspecto Físico",
-        "fechaAlta": "2011-10-17 15:10:29.0",
-        "flagEstado": "",
-        "id": 68
-    },
-    {
-        "codigoDefecto": "83",
-        "codigoFamilia": "ASF",
-        "descDefecto": "OVALADO",
-        "descFamilia": "Aspecto Físico",
-        "fechaAlta": "2011-10-17 15:10:29.0",
-        "flagEstado": "",
-        "id": 69
-    },
-    {
-        "codigoDefecto": "92",
-        "codigoFamilia": "ASF",
-        "descDefecto": "ROLLO SIN IDENTIFICACIÓN",
-        "descFamilia": "Aspecto Físico",
-        "fechaAlta": "2011-10-17 15:10:29.0",
-        "flagEstado": "",
-        "id": 72
-    },
-    {
-        "codigoDefecto": "180",
-        "codigoFamilia": "ASF",
-        "descDefecto": "DOBLEZ PAÑUELO",
-        "descFamilia": "Aspecto Físico",
-        "fechaAlta": "2011-10-17 15:10:29.0",
-        "flagEstado": "",
-        "id": 96
-    },
-    {
-        "codigoDefecto": "192",
-        "codigoFamilia": "ASF",
-        "descDefecto": "ROLLO MAL IDENTIFICADO",
-        "descFamilia": "Aspecto Físico",
-        "fechaAlta": "2011-10-17 15:10:29.0",
-        "flagEstado": "",
-        "id": 99
-    },
-    {
-        "codigoDefecto": "573",
-        "codigoFamilia": "ASF",
-        "descDefecto": "ROLLO CAZUELA",
-        "descFamilia": "Aspecto Físico",
-        "fechaAlta": "2011-10-17 15:10:29.0",
-        "flagEstado": "",
-        "id": 184
-    },
-    {
-        "codigoDefecto": "593",
-        "codigoFamilia": "ASF",
-        "descDefecto": "DEVANADO",
-        "descFamilia": "Aspecto Físico",
-        "fechaAlta": "2011-10-17 15:10:29.0",
-        "flagEstado": "",
-        "id": 191
-    },
-    {
-        "codigoDefecto": "774",
-        "codigoFamilia": "ASF",
-        "descDefecto": "ROLLO CORCHOLATA",
-        "descFamilia": "Aspecto Físico",
-        "fechaAlta": "2011-10-17 15:10:29.0",
-        "flagEstado": "",
-        "id": 242
     }
 ]
+
 window.addEventListener('load', function () {
 
     $('#table1').DataTable({
@@ -255,16 +65,16 @@ window.addEventListener('load', function () {
             $(row).attr('draggable', 'true');
         },
         drawCallback: function () {
-            // Add HTML5 draggable event listeners to each row
-            rows = document.querySelectorAll('#table1 tbody tr');
-            [].forEach.call(rows, function (row) {
 
+            var trs = $(this).DataTable().table().container().querySelectorAll('tbody tr');
+            [].forEach.call(trs, function (row) {
                 row.addEventListener('dragstart', handleDragStart, false);
                 row.addEventListener('dragenter', handleDragEnter, false)
                 row.addEventListener('dragover', handleDragOver, false);
                 row.addEventListener('dragleave', handleDragLeave, false);
                 row.addEventListener('drop', handleDrop, false);
                 row.addEventListener('dragend', handleDragEnd, false);
+                row.addEventListener('click', handleClick, false);
             });
         }
     });
@@ -282,36 +92,47 @@ window.addEventListener('load', function () {
             $(row).attr('draggable', 'true');
         },
         drawCallback: function () {
-            // Add HTML5 draggable event listeners to each row
-            rows2 = document.querySelectorAll('#table2 tbody tr');
-            [].forEach.call(rows2, function (row) {
+
+            var trs = $(this).DataTable().table().container().querySelectorAll('tbody tr');
+            [].forEach.call(trs, function (row) {
                 row.addEventListener('dragstart', handleDragStart, false);
                 row.addEventListener('dragenter', handleDragEnter, false)
                 row.addEventListener('dragover', handleDragOver, false);
                 row.addEventListener('dragleave', handleDragLeave, false);
                 row.addEventListener('drop', handleDrop, false);
                 row.addEventListener('dragend', handleDragEnd, false);
+                row.addEventListener('click', handleClick, false);
             });
         }
     });
 
 })
 
+function handleClick(e) {
+    this.classList.toggle('over');
+}
+
 function handleDragStart(e) {
-    // this / e.target is the source node.
 
-    // Set the source row opacity
-    this.style.opacity = '0.4';
+    var table = $(this).closest('table')
+    var selected = table.find('.over')
 
-    // Keep track globally of the source row and source table id
-    dragSrcRow = this;
-    srcTable = this.parentNode.parentNode.id
+    selected.length
+        ? [].forEach.call(selected, function (ele) {
+
+            ele.style.opacity = '0.4';
+        })
+        : this.style.opacity = '0.4';
+
+    dragSrcRow = {
+        data: $(table).DataTable().rows(selected.length ? selected : this).data(),
+        elements: selected.length ? selected : this
+    };
+
+    srcTable = table.attr('id')
 
     // Allow moves
     e.dataTransfer.effectAllowed = 'move';
-
-    // Save the source row html as text
-    e.dataTransfer.setData('text', e.target.outerHTML);
 
 }
 
@@ -327,26 +148,24 @@ function handleDragOver(e) {
 }
 
 function handleDragEnter(e) {
-    // this / e.target is the current hover target.  
 
-    // Get current table id
-    var currentTable = this.parentNode.parentNode.id
+    var currentTable = $(this).closest('table').attr('id');
 
-    // Don't show drop zone if in source table
     if (currentTable !== srcTable) {
         this.classList.add('over');
     }
 }
 
 function handleDragLeave(e) {
-    // this / e.target is previous target element.
 
-    // Remove the drop zone when leaving element
-    this.classList.remove('over');
+    var currentTable = $(this).closest('table').attr('id');
+
+    if (currentTable !== srcTable) {
+        this.classList.remove('over');
+    }
 }
 
 function handleDrop(e) {
-    // this / e.target is current target element.
 
     if (e.stopPropagation) {
         e.stopPropagation(); // stops the browser from redirecting.
@@ -354,39 +173,35 @@ function handleDrop(e) {
 
     // Get destination table id, row
     var dstTable = $(this).closest('table').attr('id');
-    var dstRow = $(this).closest('tr');
 
     // No need to process if src and dst table are the same
     if (srcTable !== dstTable) {
 
-        // Get source transfer data
-        var srcData = e.dataTransfer.getData('text');
-
         // Add row to destination Datatable
-        $('#' + dstTable).DataTable().row.add($(srcData)).draw();
+        $('#' + dstTable).DataTable().rows.add(dragSrcRow.data).draw();
 
         // Remove ro from source Datatable
-        $('#' + srcTable).DataTable().row(dragSrcRow).remove().draw();
+        $('#' + srcTable).DataTable().rows(dragSrcRow.elements).remove().draw();
 
     }
     return false;
 }
 
-function handleDragEnd(e) {
-    // this/e.target is the source node.
+function handleDragEnd() {
 
     // Reset the opacity of the source row
     this.style.opacity = '1.0';
+    this.classList.remove('over');
 
-    // Clear 'over' class from both tables
-    // and reset opacity
-    [].forEach.call(rows, function (row) {
-        row.classList.remove('over');
-        row.style.opacity = '1.0';
-    });
+    var table = $(this).closest('table')
+    var selected = table.find('.over')
 
-    [].forEach.call(rows2, function (row) {
-        row.classList.remove('over');
-        row.style.opacity = '1.0';
-    });
+    selected.length
+        ? [].forEach.call(selected, function (r) {
+            r.style.opacity = '1.0';
+            r.classList.remove('over');
+        })
+        : null
+
+
 }
